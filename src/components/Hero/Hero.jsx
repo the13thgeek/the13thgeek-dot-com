184de8907fc55f1bec5abd72d634a67d3793a627
the13thgeek <aldreich.aguilar@yahoo.com>
2024-08-10 18:35:55 -0400

    Added functionality to navigation bar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './Hero.css';
 import profile_img from '../../assets/profile_img.svg';
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img} alt ="" />
         <h1><span>I'm @the13thgeek,</span> frontend developer based in Toronto.</h1>
         <p>The quick brown fox jumps over the lazy dog.</p>
         <div className="hero-action">
             <div className="hero-connect">
-                Connect with me
+              <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink>
             </div>
             <div className="hero-resume">
                 My resume
