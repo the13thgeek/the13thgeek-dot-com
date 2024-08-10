e1d04bce271fdbf58160453db6039f7599471be7
the13thgeek <aldreich.aguilar@yahoo.com>
2024-08-10 18:56:58 -0400

    added contact form mailer code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,6 +6,31 @@ import location_icon from '../../assets/location_icon.svg';
 import call_icon from '../../assets/call_icon.svg';
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "89d5c5ae-07af-4139-9593-2ce2d4c771e8");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+        };
+
   return (
     <div id='contact' className='contact'>
         <div className="contact-title">
@@ -31,7 +56,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <form className="contact-right">
+            <form onSubmit={onSubmit} className="contact-right">
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='Enter your name' name='name' />
                 <label htmlFor="">Your Email</label>
