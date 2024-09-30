2e77c72595ce4b143c6f17ecca01506862cfed1a
the13thgeek <aldreich.aguilar@yahoo.com>
2024-09-30 16:53:52 -0400

    Updates

--- assets/browser-DIZmB0LE.js
+++ assets/browser-DIZmB0LE.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-hYfsAfUb.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-BUk9KBvm.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
