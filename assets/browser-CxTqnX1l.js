a59841016ba855b97ee46e0fe1a0d718101847e9
the13thgeek <aldreich.aguilar@yahoo.com>
2024-10-02 23:00:20 -0400

    Updates

--- assets/browser-CxTqnX1l.js
+++ assets/browser-CxTqnX1l.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-kmaT6RIZ.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-BZ9LcPjy.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
