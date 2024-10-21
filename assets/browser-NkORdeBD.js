0014f564fe47ff1d163b812b32a408c48172da1c
the13thgeek <aldreich.aguilar@yahoo.com>
2024-10-21 00:36:55 -0400

    Updates

--- assets/browser-NkORdeBD.js
+++ assets/browser-NkORdeBD.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-DgbiK2F4.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-Doe0W4EE.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
